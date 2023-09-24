3e1fa6186a5716763d9d2251561228ab17d2c019
Alec Hampton <admin@thewebfromscratch.com>
2023-09-24 12:14:37 -0600

    Red

--- TestTestRunner.go
+++ TestTestRunner.go
@@ -1,8 +1,18 @@
 package main
 
 func main() {
+	testWasRun()
+	testSetUp()
+}
+
+func testWasRun() {
 	test := NewTestCase("TestMethod")
 	Assert(!test.wasRun)
 	test.Run()
 	Assert(test.wasRun)
+}
+func testSetUp() {
+	test := NewTestCase("TestMethod")
+	test.run()
+	Assert(test.wasSetUp)
 }
